dac4d1094edbaf6e4ffc274cee022e39e3922594
Gabriel Hamasaki <gabrielhamasaki82@outlook.com>
2023-09-30 21:53:01 -0300

    update

--- config.fish
+++ config.fish
@@ -8,49 +8,16 @@ starship init fish | source
 export PATH="$PATH:$HOME/.rbenv/bin"
 eval "$(rbenv init -)"
 
-# PhP
-export PATH="$PATH:$HOME/.config/composer/vendor/bin/"
-
-# JAVA
-export JAVA_HOME="/home/gabriel/.java/jdk17"
-export PATH="$PATH:$JAVA_HOME/bin"
-
 # Go
-export PATH="$PATH:$HOME/.gvm/scripts/gvm"
+export GOROOT="/home/gabrielhamasaki/.gvm/versions/go1.21.1.linux.amd64"
+export PATH="/home/gabrielhamasaki/.gvm/versions/go1.21.1.linux.amd64/bin:$PATH"
 
 # Rust
 export PATH="$PATH:$HOME/.cargo/bin"
 
-# Androind
-export ANDROID_HOME="/home/gabriel/Android/Sdk"
-export PATH="$PATH:$ANDROID_HOME/emulator"
-export PATH="$PATH:$ANDROID_HOME/tools"
-export PATH="$PATH:$ANDROID_HOME/tools/bin"
-export PATH="$PATH:$ANDROID_HOME/platform-tools"
-export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
-
-# Deno
-export PATH="$PATH:$HOME/.dvm/bin"
+# Yarn bin
 export PATH="$PATH:$HOME/.yarn/bin"
 
-# LVIM
-export PATH="$PATH:$HOME/.local/bin"
-
-# Aliases
-
-alias art="php artisan"         
-
-function v -d "nvim"
-    if count $argv > /dev/null
-        nvim $argv
-    end
-    nvim 
-end
-
-function new-project -d "Create a new project laravel with typescript and react/vue/blade"
-    laravel new $argv[1] --typescript --breeze --stack=$argv[2] --git --no-interaction
-end
-
-# bun
+# Bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
