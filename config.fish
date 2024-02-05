6cdbc4fe58e2ead2704582ab8d31fe7f6547b066
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-02-05 10:24:03 -0300

    update fish config 2024

--- config.fish
+++ config.fish
@@ -4,29 +4,25 @@ end
 
 starship init fish | source
 
-# Java
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export PATH="$PATH:/bin/squashfs-root/usr/bin/"
 
 # PhP
 export PATH="$PATH:$HOME/.config/composer/vendor/bin/"
-
-# Ruby
-export PATH="$PATH:$HOME/.rbenv/bin"
-eval "$(rbenv init -)"
-
-# Go
-export GOROOT="/home/gabrielhamasaki/.gvm/versions/go1.21.1.linux.amd64"
-export PATH="/home/gabrielhamasaki/.gvm/versions/go1.21.1.linux.amd64/bin:$PATH"
-
-# Rust
-export PATH="$PATH:$HOME/.cargo/bin"
-
-# Yarn bin
 export PATH="$PATH:$HOME/.yarn/bin"
 
-# Bun
+# GO
+export PATH="$PATH:/usr/local/go/bin"
+
+# bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 
+# ALIASES
 
+alias n="nvim"
 alias art="php artisan"
+alias p="pnpm"
+alias ga="git add ."
+alias gs="git status -s"
+alias gc = "git commit -m"
+alias checkout="git checkout"
