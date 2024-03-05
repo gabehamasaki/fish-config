60394d055fb8c5dd946e5e5c14a0a041bbfcb550
Gabriel Hamasaki <gabriel.hamasaki@exata.it>
2024-03-05 08:48:37 -0300

    Update fish

--- config.fish
+++ config.fish
@@ -2,6 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+starship init fish | source
+source ~/.asdf/asdf.fish
+
 export PATH="$PATH:/bin/squashfs-root/usr/bin/"
 
 # PhP
@@ -25,7 +28,3 @@ alias gs="git status -s"
 alias gc="git commit -m"
 alias gp="git push"
 alias checkout="git checkout"
-
-# tabtab source for packages
-# uninstall by removing these lines
-[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
